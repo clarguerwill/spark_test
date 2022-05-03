d8f7b541de975926538c4720bb2df276fab65fe7
clarguerwill <clarguerwill@gmail.com>
2022-05-03 17:36:50 -0400

    Parsed data

--- ofac.py
+++ ofac.py
@@ -1,7 +1,10 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType,StructField, StringType
+import json
+import xmltodict
 #com.databricks:spark-xml_2.11:0.12.0
 # spark-submit --master local[*] --py-files="Users/clarisseguerin-williams/Documents/ofac_data/optimize-spark.py" test.py
+
 
     
 def get_uk_treasury(file_path="uk_treasury.csv"):
@@ -13,16 +16,54 @@ def get_uk_treasury(file_path="uk_treasury.csv"):
     cols = []
     for col in headers:
         cols.append(StructField(col, StringType(),True))
-
     schema = StructType(cols)
-
     df = spark.createDataFrame(data, schema)
-    return df 
+    return df
 
 
 
-def get_ofac(file_path="uk_treasury.csv"):
-    pass
+def get_ofac_json(file_path="ofac.xml"):
+    
+    with open(file_path) as xml_file:
+        data_dict = xmltodict.parse(xml_file.read())
+    xml_file.close()
+
+    json_data = json.dumps(data_dict)
+    file_path = file_path.replace(".xml", ".json")
+    with open(file_path, "w") as json_file:
+        json_file.write(json_data)
+
+    json_file.close()
+
+
+
+def get_ofac(file_path="ofac.xml"):
+    get_ofac_json(file_path)
+    file_path = file_path.replace(".xml", ".json")
+    df = spark.read.json(file_path)
+    return df
+
+
+
+def main():
+
+    global spark
+    spark = SparkSession.builder \
+        .master("local[*]") \
+        .appName("Test") \
+        .getOrCreate() 
+    
+    uk = get_uk_treasury()
+    ofac = get_ofac()
+    uk.printSchema()
+    ofac.printSchema()
+
+
+
+if __name__ == "__main__":
+    main()
+
+
 
 """
 sdnEntry
@@ -45,19 +86,3 @@ sdnEntry
 
 
 """
-
-
-def main():
-    global spark
-    spark = SparkSession.builder \
-        .master("local[*]") \
-        .appName("Test") \
-        .getOrCreate() 
-
-    # uk = get_uk_treasury()
-
-
-
-
-if __name__ == "__main__":
-    main()

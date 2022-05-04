f7da6e9e029ed0573c19af61c7f4ac0ae59b1afd
clarguerwill <clarguerwill@gmail.com>
2022-05-04 19:48:03 -0400

    UK Schema Info

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType,StructField, StringType
+
 import json
 import xmltodict
+import re
 # com.databricks:spark-xml_2.11:0.12.0
 # spark-submit --master local[*] --py-files="Users/clarisseguerin-williams/Documents/ofac_data/optimize-spark.py" test.py
 
-
     
 def get_uk_treasury(file_path="uk_treasury.csv"):
+   
     data = spark.sparkContext.textFile(file_path)
-    headers = data.collect()[1].split(",")
+
+    cols = data.collect()[1].split(",")
+    headers = list(map(lambda x : re.sub("[^0-9a-zA-Z]+", "", x), cols))
+
     data = data.zipWithIndex().filter(lambda row_index: row_index[1] > 1).keys()
     data = data.map(lambda row : row.split(","))
 
@@ -17,10 +22,10 @@ def get_uk_treasury(file_path="uk_treasury.csv"):
     for col in headers:
         cols.append(StructField(col, StringType(),True))
     schema = StructType(cols)
+
     df = spark.createDataFrame(data, schema)
+
     return df
-
-
 
 def get_ofac_json(file_path="ofac.xml"):
     
@@ -38,13 +43,31 @@ def get_ofac_json(file_path="ofac.xml"):
     json_file.close()
 
 
-
 def get_ofac(file_path="ofac.xml"):
     get_ofac_json(file_path)
     file_path = file_path.replace(".xml", ".json")
     df = spark.read.json(file_path, primitivesAsString='true')
     return df
     
+
+def print_counts(df, col):
+    cntd = str(df.select(col).distinct().count())
+    cnt = str(df.select(col).count())
+    print(f"-------------\nDistinct Cnt = {cntd}\nCount={cnt}")
+
+
+def show_sample(df, col, n=10, f=.01):
+    df.select(col) \
+        .filter(f"{col} IS NOT NULL") \
+        .sample(withReplacement=True, fraction=f) \
+        .show(n, False)
+
+
+def show_distinct_vals(df, col):
+    df.select(col) \
+        .distinct() \
+        .collect()
+
 
 def main():
 
@@ -55,10 +78,18 @@ def main():
         .getOrCreate() 
     
     uk = get_uk_treasury()
-    ofac = get_ofac()
+    #ofac = get_ofac()
+    #uk.printSchema()
+    #ofac.printSchema()
+    uk.select("AliasType").show()
 
-    uk.printSchema()
-    ofac.printSchema()
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
